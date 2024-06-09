705560391de5c698d70b3489fcf1f943c102e7e9
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-09 21:14:13 +0400

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project-saba123';
-}
+  title = 'sashinao2';
+  
+
+  
+   name: string = "John";
+   lastName: string = "Doe";
+  
+
+   isVisible: boolean = true;
+
+   toggleVisibility() {
+     this.isVisible = !this.isVisible;
+   }
+ }
+
+
+
+
+
+
+
+
