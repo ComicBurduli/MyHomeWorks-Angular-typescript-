a019b125059361f7910ada542dfd1b2cb184bd87
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-09 21:16:24 +0400

    homework1

---
+++ src/app/component-main/component-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-main.component.html',
+  styleUrl: './component-main.component.css'
+})
+export class ComponentMainComponent {
+
+}
